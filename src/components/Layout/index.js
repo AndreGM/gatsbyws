911e8580745594ca5d3ec746e1e129793cf65426
andregm <andre@appdev.com.br>
2021-03-03 16:31:01 -0300

    image

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import {Link} from "gatsby"
 import { title } from './layout.module.css'
+import { StaticImage } from "gatsby-plugin-image"
 
 const Layout = ({children, pageTitle}) => {
   return(
@@ -9,6 +10,7 @@ const Layout = ({children, pageTitle}) => {
       <Link to='/about'> Go to About page </Link>
       <hr/>
       <h1 className={title}>{pageTitle}</h1>
+      <StaticImage src="https://pbs.twimg.com/media/Evkn6fRWgAgxOWe?format=jpg&name=small" alt="image" />
       { children }
     </div>
   )

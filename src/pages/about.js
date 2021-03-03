c84a64ed16aaa09be1ea65d797adad5eec00c79f
andregm <andre@appdev.com.br>
2021-03-03 15:28:04 -0300

    Links

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,14 @@
 import * as React from "react"
-
+import {Link} from "gatsby"
 
 // markup
 const AboutPage = () => {
   return (
-    <h1> About</h1>
+    <>
+      <Link to='/'> Go to Home page </Link>
+      <Link to='/about'> Go to About page </Link>
+      <h1> About</h1>
+    </>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from "react"
+import { Link } from "gatsby"
 
 
 // markup
 const IndexPage = () => {
   return (
-    <h1> Welcome to my site</h1>
+    <>
+      <Link to='/'> Go to Home page </Link>
+      <Link to='/about'> Go to About page </Link>
+      <h1> Welcome to my site</h1>
+    </>
   )
 }
 
